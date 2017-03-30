150890de5ae4a73e9f086dc0ece49efe1de8d797
Steven Shaw <steven@steshaw.org>
2017-03-30 12:10:17 +1000

    Summing the bits

--- week1/assignment.erl
+++ week1/assignment.erl
@@ -6,10 +6,18 @@
   , perimeter_test/0
   , enclose/1
   , enclose_test/0
+  , bit_string/1
+  , bits1/1
+  , bits2/1
+  , bits_test/1
+  , bits1_test/0
+  , bits2_test/0
   ]).
 
 %
-% Shapes are represented using the following:
+% Shapes.
+%
+% Here we use the following representation:
 %
 %   {circle, R}
 %   {rectangle, H, W}
@@ -73,3 +81,55 @@ enclose_test() ->
   R2 = enclose(rectangle1()) == rectangle1(),
   R3 = enclose(triangle1()) == {rectangle, 5.0, 2.4},
   [R1, R2, R3].
+
+%
+% Summing the bits.
+%
+
+% Extra function for fun and profit. Converts positive integer % to it's
+% binary % reprentation as a string.
+% Implementing this gave me confidence that I had the algorighm correct.
+% I also compared this in the shell against `integer_to_binary(N, 2)`.
+bit_string(0) -> "";
+bit_string(N) when N > 0 ->
+  Bit = if
+    (N rem 2) == 1 -> "1";
+    true -> "0"
+  end,
+  bit_string(N div 2) ++ Bit.
+
+% "direct" recursive.
+bits1(0) -> 0;
+bits1(N) when N > 0 ->
+  (N rem 2) + bits1(N div 2).
+
+% tail recursive.
+bits2(N) when N >= 0 -> bits2(N, 0).
+bits2(0, Sum) -> Sum;
+bits2(N, Sum) ->
+  bits2(N div 2, Sum + (N rem 2)).
+
+%
+% Which do you think is better? Why?
+%
+% bits2, the tail recursive version, is a better program
+% because it runs in constant (stack) space.
+%
+% However, bits1, is easier to understand and provides
+% a useful test "oracle". i.e. it can calculate the correct
+% answers to compare against the more efficient version. This
+% is particularly useful for QuickCheck style testing.
+%
+
+% expected result is [true,true,true,true,true].
+bits_test(BitsF) ->
+  [ BitsF(0) == 0
+  , BitsF(1) == 1
+  , BitsF(2) == 1
+  , BitsF(3) == 2
+  , BitsF(4) == 1
+  , BitsF(255) == 8
+  ].
+
+bits1_test() -> bits_test(fun bits1/1).
+bits2_test() -> bits_test(fun bits2/1).
