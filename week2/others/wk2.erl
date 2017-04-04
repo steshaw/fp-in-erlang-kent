e92440aaa8b36081ef0b89e51f01c2ace4aa1edb
Steven Shaw <steven@steshaw.org>
2017-04-04 19:14:23 +1000

    Another student's interesting solution

---
+++ week2/others/wk2.erl
@@ -0,0 +1,48 @@
+%% From https://hastebin.com/ukevotoyel.erl
+
+%% Week 2 Stuff
+
+-module(wk2).
+
+-export([head/1,tail/1,second/1,sum/1,sumTail/1,product/1,productTail/1,max/1,maxTail/1]).
+
+%% head and tail
+head([]) -> [];
+head([X|_Xs]) -> X.
+tail([]) -> [];
+tail([_X|Xs]) -> Xs.
+
+%% second.
+second([X|Xs]) -> head(tail([X|Xs])).
+
+%% sum: add up all the elements in a list using direct recursion.
+sum([]) -> 0;
+sum([X|Xs]) -> X + sum(Xs).
+
+%%sumTail: add up all the elements in a list using tail recursion.
+sumTail([]) -> 0;  %% return 0 for an empty list like sum does.
+sumTail([X|Xs]) -> sumTail([X|Xs],0).  %% convert to 2 arguments by adding accumulator.
+sumTail([],S) -> S;  %% return the sum when list exhausted.
+sumTail([X|Xs],S) -> sumTail(Xs,S+X).  %% add in X and repeat.
+
+
+%% product: return the product of a list of numbers.
+product([]) -> 1;
+product([X|Xs]) -> X * product(Xs).
+%%productTail: return the product of a list of numbers using tail recursion.
+productTail(Xs) -> productTail(Xs,1).
+productTail([],P) -> P;
+productTail([X|Xs],P) -> productTail(Xs,P*X).
+
+
+%% max: return the highest value in a list.
+max([]) -> [];
+max([X|Xs]) -> case length([X|Xs]) of
+    1 -> X;  %% return the surviving value
+    _Else -> max([max(X,head(Xs))|tail(Xs)])  %% max of 1st 2 elements then rest of list recursively.
+end.
+%% maxTail: return highest value in a list via tail recursion.
+maxTail([]) -> [];
+maxTail([X|Xs]) -> maxTail(X,Xs).  %% Use 1st argument for storing max so far.
+maxTail(M,[]) -> M;  %% return the highest value when list exhausted.
+maxTail(M,[X|Xs]) -> maxTail(max(M,X),Xs).  %% Check head against M then repeat.
