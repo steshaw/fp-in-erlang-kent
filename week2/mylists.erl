1999177954d72eb5f52cb21d8a90cf17d99a37e1
Steven Shaw <steven@steshaw.org>
2017-04-04 18:05:52 +1000

    Week 2

---
+++ week2/mylists.erl
@@ -0,0 +1,25 @@
+-module(mylists).
+-export(
+ [ product/1
+ , product_tr/1
+ , maximum/1
+ , maximum_tr/1
+ ]
+).
+
+product([]) ->
+  % The product of an empty list is usually taken to be 1: why?
+  % The number 1 is the multiplicative identity.
+  1;
+product([X | XS]) -> X * product(XS).
+
+product_tr(XS) -> product_tr(XS, 1).
+product_tr([], Product) -> Product;
+product_tr([X | XS], Product) -> product_tr(XS, X * Product).
+
+maximum([X]) -> X;
+maximum([X | XS]) -> max(X, maximum(XS)).
+
+maximum_tr([X | XS]) -> maximum_tr(XS, X).
+maximum_tr([], Maximum) -> Maximum;
+maximum_tr([X | XS], Maximum) -> maximum_tr(XS, max(X, Maximum)).
