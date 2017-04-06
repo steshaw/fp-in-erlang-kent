4a8f47ac08be94e5887388588c130cda811512c8
Steven Shaw <steven@steshaw.org>
2017-04-06 19:00:49 +1000

    Add docs and implementation of `map` and `filter`.

--- week2/mylists.erl
+++ week2/mylists.erl
@@ -63,9 +63,12 @@ evens([N | NS]) ->
     false -> evens(NS)
   end.
 
+%
+% @doc
 % The median of a list of numbers.
 % This is the middle element when the list is ordered (if the list is of
 % even length you should average the middle two).
+%
 -spec median(nonempty_list(number())) -> number().
 median([_ | _] = Ns) -> % only works for non-empty lists.
   Sorted = sort(Ns),
@@ -83,7 +86,11 @@ median([_ | _] = Ns) -> % only works for non-empty lists.
       nth(Middle, Sorted)
   end.
 
-% Expected: [true, true].
+%
+% @doc Unit test for `median'.
+% @see median/1
+% @returns Expected: [true, true].
+%
 median_test() ->
   [ median([4, 3, 2, 1]) == average(2, 3)
   , median([3, 2, 1]) == 2
@@ -97,12 +104,19 @@ average(A, B) -> (A + B) / 2.
 
 % Other uses functions.
 
+%
+% @doc
 % Sort the list.
+%
 sort([]) -> [];
 sort([X]) -> [X];
 sort([X | Xs]) -> insert(X, sort(Xs)).
 
-% Expect list of trues.
+%
+% @doc Unit test for `sort'.
+% @see sort/1
+% @returns Expect list of trues.
+%
 sort_test() ->
   [ sort([]) == []
   , sort([1]) == [1]
@@ -112,7 +126,10 @@ sort_test() ->
   , sort([2,1,3]) == [1,2,3]
   ].
 
+%
+% @doc
 % Insert X into a sorted list.
+%
 insert(X, []) -> [X];
 insert(X, [Y | Ys] = Xs) ->
   case X < Y of
@@ -120,11 +137,18 @@ insert(X, [Y | Ys] = Xs) ->
     false -> [Y | insert(X, Ys)]
   end.
 
+%
+% @doc
 % The nth element of the list.
+%
 nth(1, [X | _ ]) -> X;
 nth(N, [_ | Xs]) -> nth(N - 1, Xs).
 
-% Expecting [true, true, true].
+%
+% @doc Unit test for `nth'.
+% @see nth/2
+% @returns Expecting [true, true, true].
+%
 nth_test() ->
   [ nth(1, [1, 2, 3]) == 1
   , nth(2, [1, 2, 3]) == 2
@@ -132,23 +156,31 @@ nth_test() ->
 %  , nth(4, [1, 2, 3]) == 1/0
   ].
 
+%
+% @doc
 % The modes of a list of numbers.
 % This is a list consisting of the numbers that occur most frequently
 % in the list; if there is is just one, this will be a list with one
 % element only
 %
+% @end
 % Sigh, this is a very slow implementation because of the number
 % of traversals through the list.
+%
 -spec modes([number()]) -> [number()].
 modes([]) -> [];
 modes(Ns) ->
   FrequencyMaps = frequencies(Ns),
-  Frequencies = lists:map(fun ({_, F}) -> F end, FrequencyMaps),
+  Frequencies = map(fun ({_, F}) -> F end, FrequencyMaps),
   MaximumFrequency = maximum(Frequencies),
-  MaximumFrequencyMaps = lists:filter(fun ({_, F}) -> F == MaximumFrequency end, FrequencyMaps),
-  lists:map(fun ({N, _}) -> N end, MaximumFrequencyMaps).
+  MaximumFrequencyMaps = filter(fun ({_, F}) -> F == MaximumFrequency end, FrequencyMaps),
+  map(fun ({N, _}) -> N end, MaximumFrequencyMaps).
 
-% Expect a list of trues.
+%
+% @doc Unit test for `modes'.
+% @see modes/1
+% @returns a list of trues.
+%
 modes_test() ->
   [ modes([]) == []
   , modes([1]) == [1]
@@ -157,21 +189,51 @@ modes_test() ->
   , modes([6,4,5,9,2,5,6]) == [5, 6]
   ].
 
+%
+% @doc
 % The frequencies/occurences of each unique element of a list.
 % Returns a list of tuples {N, occurrences(N)}.
+%
 frequencies(Ns) ->
   Us = unique(Ns),
-  lists:map(fun(N) -> {N, occurences(N, Ns)} end, Us).
+  map(fun(N) -> {N, occurences(N, Ns)} end, Us).
 
-% unique - just the unique elements of the list.
+%
+% @doc
+% Return only the unique elements of a list.
+%
 unique(Xs) -> uniq(sort(Xs)).
 
-% uniq - unique elements of sorted list.
+%
+% @doc
+% Removes adjacent duplicate elements.
+%
 uniq([]) -> [];
 uniq([X, X | Xs]) -> uniq([X | Xs]);
 uniq([X | Xs]) -> [X | uniq(Xs)].
 
-% occurences: How many times does a value occur in the list?
+%
+% @doc
+% Count the occurrences of a value in a list.
+%
 occurences(_, []      ) -> 0;
 occurences(X, [X | Xs]) -> 1 + occurences(X, Xs);
 occurences(X, [_ | Xs]) -> occurences(X, Xs).
+
+%
+% @doc
+% non-tail-recursive map/transform.
+%
+map(_, []) -> [];
+map(F, [X | Xs]) -> [F(X) | map(F, Xs)].
+
+%
+% @doc
+% non-tail-recursive filter.
+%
+filter(_, []) -> [];
+filter(F, [X | Xs]) ->
+  case F(X) of
+    true  -> [X | filter(F, Xs)];
+    false -> filter(F, Xs)
+  end.
